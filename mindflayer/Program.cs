ea3e418b10e4df0ca04ef81c8764fe7a584287ed
Ian <ibaaske@liquidus.net>
2018-11-21 13:11:40 -0600

    ~~~

---
+++ mindflayer/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace mindflayer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
